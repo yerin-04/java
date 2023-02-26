98d67485d93c156f88b149192d0e63326a5e9f1c
유예린 <irene4311@naver.com>
2023-02-26 18:55:31 +0900

    java 문법 변수

---
+++ pjt.Test/src/pjt/Test/MainClass.java
@@ -0,0 +1,13 @@
+package pjt.Test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		int j;
+		j = 120;
+		
+		System.out.println("i + j = " + (i+j));
+	}
+}
