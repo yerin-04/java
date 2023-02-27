f34b8b0e5cf90cc99977dda5aeb2d1db24e82e87
유예린 <irene4311@naver.com>
2023-02-28 00:56:31 +0900

    java 강의 6강 연산자

---
+++ Pjt4/src/JavaPjt4.java
@@ -0,0 +1,26 @@
+
+public class JavaPjt4 {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * 복합대입연산자
+		 * x += 10 은 x = x + 10 랑 같은 의미
+		 */
+		int x = 10;
+		System.out.println("x += 10 : " + (x += 10));
+		
+		//관계연산자
+		// != a!=b a와 b가 같지 않으면 참
+		int y = 20;
+		System.out.println("x==y: " + (x==y));
+		System.out.println("x!=y: " + (x!=y));
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ Pjt4/src/Pjt4.java
@@ -0,0 +1,20 @@
+
+public class Pjt4 {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * x += 10 는 x = x + 10; 과 같은 의미
+		 */
+		int x =10;
+		System.out.println("x += 10 : " + (x += 10));
+		
+		
+		
+	}
+	
+	
+	
+	
+}
