2d147a9b74259ff0cc56d3b0e80144454984100d
유예린 <irene4311@naver.com>
2023-02-28 00:06:40 +0900

    java강의 특수 문자와 서식 문자

---
+++ pjt3/src/pjt3/JavaPjt3.java
@@ -0,0 +1,39 @@
+package pjt3;
+
+public class JavaPjt3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Good\tMoring~");
+		System.out.println("Good\nMoring~");
+		System.out.println("Good\'Moring~\'");
+		System.out.println("Good\"Moring\"");
+		
+		//배고프당
+		/*
+		 * %d 10진수
+		 * %o 8진수
+		 * %x 16진구
+		 * %c 문자
+		 * %s 문자열
+		 * %f 실수
+		 * 
+		 */
+	
+		
+		System.out.printf("오늘의 날씨는 %d도 입니다.", 8);
+		
+		char c1 = 'a';
+		char c2 = 'A';
+		
+		
+		System.out.printf("소문자 \'%c\'의 대문자는 \'%c\'입니다.\n\n", c1, c2);
+		
+		
+		
+		
+	}
+	
+	
+	
+}
