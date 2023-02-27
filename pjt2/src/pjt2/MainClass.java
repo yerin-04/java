71a40d36b4d01e0555299cdc46bae72f39caf6e9
유예린 <irene4311@naver.com>
2023-02-27 18:15:00 +0900

    java강의 기본자료형 char int double boolean String // stirng에서 S는 대문자

---
+++ pjt2/src/pjt2/MainClass.java
@@ -0,0 +1,27 @@
+package pjt2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		char c = 'a';
+		System.out.println("c = "+c);
+		
+		int i = 10;
+		System.out.println("i =" + i);
+		
+		double d = 10.123;
+		System.out.println("d = " + d);
+		
+		boolean b = false;
+		System.out.println("b = " + b);
+		
+		String s = "Hello Java World!!!!!!";
+		System.out.println("s = " + s);
+		
+		s = "Good";
+		System.out.println("s = " + s);
+		
+	}
+	
+}
