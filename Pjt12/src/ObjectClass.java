3bcc5019c27dee1ee9346ac1f07378ebef55a6f2
유예린 <irene4311@naver.com>
2023-03-16 13:57:12 +0900

    JAVA강의 생성자와 소멸자 그리고 this 키워드

---
+++ Pjt12/src/ObjectClass.java
@@ -0,0 +1,9 @@
+
+public class ObjectClass {
+
+	public ObjectClass(String s, int[] iArr) {
+		System.out.println("-- ObjectClass() --");
+		System.out.println(" s----> " +s);
+		System.out.println("iArr ---->" + iArr);
+	}
+	}
---
+++ Pjt12/src/Pjt12.java
@@ -0,0 +1,9 @@
+
+public class Pjt12 {
+
+	public static void main(String[] args) {
+		
+		int[] iArr = {10,20,30};
+		ObjectClass obj1 = new ObjectClass(" Hello", iArr);
+	}
+}
