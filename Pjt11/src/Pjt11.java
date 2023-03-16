63f68a47a1087e668db57cc996f10f3d386ebd8d
유예린 <irene4311@naver.com>
2023-03-16 13:32:18 +0900

    JAVA강의 객체와 메모리 객체는 메모리에 동적으로 생성되며, 객체가 더 이상 필요 없게 되면 GC에 의해서 제거된다. 레퍼런스에  null 저장되면 객체의 연결이 끊기며, 더 이상 객체를 이용할 수 없다.

---
+++ Pjt11/src/Pjt11.java
@@ -0,0 +1,14 @@
+
+public class Pjt11 {
+
+	public static void main(String[] args) {
+		
+		ObjectClass obj1 = new ObjectClass();
+		ObjectClass obj2 = new ObjectClass();
+		ObjectClass obj3 = new ObjectClass();
+		
+		System.out.println(" obj1 : "+ obj1);
+		System.out.println(" obj2 : "+ obj2);
+		System.out.println(" obj3 : "+ obj3);
+	}
+}
