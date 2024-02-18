2339055b9d1457c7fabe0ab4af5374b7773dbacb
유예린 <irene4311@naver.com>
2024-02-18 19:58:32 +0900

    백준 문제 풀이

---
+++ src/boj_1547.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class boj_1547 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[3];
+		arr[0] = 1;
+		arr[1] = 0;
+		arr[2] = 0;
+		
+		int num = sc.nextInt();
+		
+		for( int i = 0; i < num; i ++ ) {
+			int move1 = Integer.parseInt(sc.next()) - 1;
+			int move2 = Integer.parseInt(sc.next()) - 1;
+			
+			int tmp = 0;
+			
+			tmp = arr[move1];
+			arr[move1] = arr[move2];
+			arr[move2] = tmp;
+		}
+		
+		for (int i = 0; i<3; i ++) {
+			if( arr[i] == 1 ) {
+				System.out.println(i+1);
+			}
+		}
+		sc.close();
+	}
+}
---
+++ src/boj_19698.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class boj_19698 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		int l = sc.nextInt();
+		
+		int result = (w/l) * (h/l);
+		
+		if( result >= n ) {
+			System.out.println(n);
+		}
+		else {
+			System.out.println(result);
+		}
+	}
+}
